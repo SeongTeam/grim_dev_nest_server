1030cff56ccea0ed3320e5b21d5a7b3c16cbce16
Cori1304-Seong <andyjm7739@gmail.com>
2025-01-06 16:41:58 +0900

    feat: Add GgrimMoment

---
+++ src/util/ggrim_moment.ts
@@ -0,0 +1,14 @@
+import * as moment from 'moment';
+
+export class GgrimMoment {
+  static async getLatestMonday(): Promise<string> {
+    const today = moment();
+    const isMonday = today.isoWeekday() === 1; // 월요일인지 확인 (1 = 월요일)
+
+    const latestMonday = isMonday
+      ? today.format('YYMMDD') // 오늘이 월요일이라면 오늘 날짜 사용
+      : today.startOf('isoWeek').format('YYMMDD'); // 오늘이 월요일이 아니면 이번 주 월요일
+
+    return latestMonday;
+  }
+}
